e70ee3113d99dae77c27e9ca5c56ee0dd2d81ebb
kiichi <yougrtcap@gmail.com>
2019-11-13 11:59:04 +0900

    Add:検索したい人を表示する機能と、指定したユーザーが削除できる機能を追加

--- try.py
+++ try.py
@@ -1,18 +1,23 @@
-import sqlite3
+# def show_user_c():  # チェックリスト作成
+#     conn = sqlite3.connect('user_list_db')
+#     c = conn.cursor()
+#     c.execute(f'select * from user_list')
+#     lists = c.fetchall()
+#     conn.commit()
+#     conn.close()
+#     print(lists)
+#
+# show_user_c()
+# def show_user():  # ユーザーを表示
+# conn = sqlite3.connect('user_list_db')
+# c = conn.cursor()
+# c.execute(f'select * from user_list　where name  == "{found_name}"')
+# lists = c.fetchall()
+# conn.commit()
+# conn.close()
+# print(lists)
 
-
-def show_user_c():  # チェックリスト作成
-    conn = sqlite3.connect('user_list_db')
-    c = conn.cursor()
-    c.execute(f'select * from user_list')
-    lists = c.fetchall()
-    conn.commit()
-    conn.close()
-    li = []
-    for list in lists:
-        li.append(list[0])
-    return li
-
-
-li = show_user()
-print(li)
+abc = ('a', 'b', 'c')
+list = list(abc)
+print(list)
+print()
--- user_list.py
+++ user_list.py
@@ -4,18 +4,15 @@ import sqlite3
 def create_table():  # テーブル作成
     conn = sqlite3.connect('user_list_db')
     c = conn.cursor()
-    c.execute(f'create table user_list(name,age)')
+    c.execute(f'create table user_list(name strings,age integer )')
     conn.commit()
     conn.close()
-
-
-# def name_check():  # 名前の重複をチェック
-#     pass
 
 
 def add_user():  # 新規ユーザーを追加
     new_name = input('New user name >> ')
     new_age = input('New user age >> ')  # 重複をハジく
+    li = show_user_c()
     if new_name not in li:
         print(f'Add new user: {new_name}')
         conn = sqlite3.connect('user_list_db')
@@ -25,7 +22,6 @@ def add_user():  # 新規ユーザーを追加
         conn.close()
     else:
         print('既に存在します。')
-
 
 
 def show_user():  # ユーザーを表示
@@ -52,24 +48,58 @@ def show_user_c():  # チェックリスト作成
     return li
 
 
-li = show_user_c()
+def found_user():  # 表示したい人を検索 #PASS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!ヒットすれば表示まで。なかった場合がまだ。
+    found_name = input('User name >> ')
+    conn = sqlite3.connect('user_list_db')
+    c = conn.cursor()
+    lists = c.execute(f'select * from user_list where name == "{found_name}" ').fetchone()
 
-print(
-    f'===== Welcome to CRM Application =====\n[S]how: Show all users info\n[A]dd: Add new user\n======================================')
+    if type(lists) is tuple:
+        print(f'Name : {lists[0]} // Age : {lists[1]}')  # リスト表示
 
+    else:
+        print('そんなのナイィッ！！')
+    conn.commit()
+    conn.close()
+
+
+def delete_user():  # ユーザーの削除
+    conn = sqlite3.connect('user_list_db')
+    user_name = input('User name? >> ')
+    c = conn.cursor()
+    c.execute(f'delete from user_list where name == "{user_name}"')
+    conn.commit()
+    conn.close()
+    print(f'User "{user_name}" is Delete')
+
+
+def edit_user():  # 編集
+    conn = sqlite3.connect('user_list_db')
+    c = conn.cursor()
+    c.execute(f'update user_list set name ="pasumo" where name == "suica" ')
+    conn.commit()
+    conn.close()
+    print(f'Update user suica')
 
 def main():  # 入力表示追加
     y_command = input('Your command >> ')
     if y_command == 'a' or y_command == 'A':  # 追加
         add_user()
-    elif y_command == 's' or y_command == 'S':  # 表示　
+    elif y_command == 's' or y_command == 'S':  # 表示
         show_user()
+    elif y_command == 'f' or y_command == 'F':  # 検索
+        found_user()
+    elif y_command == 'd' or y_command == 'D':  # 削除
+        delete_user()
+    elif y_command == 'e' or y_command == 'E':  # 編集
+        edit_user()
     elif y_command == 'q' or y_command == 'Q':  # さようなら
         print('Bye !')
     else:  # 見つからない的な
         print(f'{y_command} command not found ')
         main()
 
-
 if __name__ == '__main__':
+    print(
+        f'===== Welcome to CRM Application =====\n[S]how: Show all users info\n[A]dd: Add new user [F]ind:Find user\n[D]elete: Delete user\n[E]dit: Edit a user\n======================================')
     main()
